a99371c5fecea1b3212878e81108ca313863ec2e
gkaragoz <gkaragoz96@gmail.com>
2019-01-26 15:46:42 +0300

    Create basics of GridSystem.

--- 2D-Primitive-Collision-System/Assets/Scripts/Grid.cs
+++ 2D-Primitive-Collision-System/Assets/Scripts/Grid.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Grid : MonoBehaviour {
 
     [SerializeField]
+    private int _index;
+    [SerializeField]
     private Vector2 _size;
+
+    public Vector2 Size { get => _size; set => _size = value; }
+
+    private void Awake() {
+        Size = transform.localScale;
+    }
 
 }
--- 2D-Primitive-Collision-System/Assets/Scripts/GridSystem.cs
+++ 2D-Primitive-Collision-System/Assets/Scripts/GridSystem.cs
@@ -1,23 +1,100 @@
 ﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GridSystem : MonoBehaviour {
 
-    public GameObject groundObject;
+    public bool IsCreated { get { return _createdGrids.Count > 0 ? true : false; } }
+
+    [Header("Initialization")]
+    [SerializeField]
+    private GameObject _gridPrefab;
+
+    [Header("Settings")]
+    [SerializeField]
+    private int MAP_WIDTH = 10;
+    [SerializeField]
+    private int MAP_HEIGHT = 5;
 
     [SerializeField]
-    private List<Grid> _grids = new List<Grid>();
-
+    private int ROW = 3;
     [SerializeField]
-    private float WIDTH = 10;
+    private int COLUMN = 3;
+
+    [Header("Debug")]
     [SerializeField]
-    private float HEIGHT = 10;
+    private Grid[,] grids;
 
-    private void Awake() {
-        Mesh mesh = groundObject.GetComponent<MeshFilter>().mesh;
+    private List<Grid> _createdGrids = new List<Grid>();
 
-        WIDTH = mesh.bounds.size.x;
-        HEIGHT = mesh.bounds.size.z;
+    private Transform _parent;
+
+    public void CreateGrids() {
+        grids = new Grid[ROW, COLUMN];
+
+        float SCALE_X = MAP_WIDTH / COLUMN;
+        float SCALE_Y = MAP_HEIGHT / ROW;
+
+        if (_parent == null) {
+            _parent = new GameObject("Parent").transform;
+        }
+
+        for (int ii = 0; ii < COLUMN; ii++) {
+            for (int jj = 0; jj < ROW; jj++) {
+                Grid grid = Instantiate(_gridPrefab, new Vector3(ii * SCALE_X, 0f, jj * SCALE_Y), Quaternion.identity).GetComponent<Grid>();
+                grid.Size = new Vector2(SCALE_X, SCALE_Y);
+                grid.transform.localScale = new Vector3 (grid.Size.x, 1f, grid.Size.y);
+                grid.transform.SetParent(_parent);
+
+                _createdGrids.Add(grid);
+            }
+        }
+    }
+
+    public void DeleteGrids() {
+        for (int ii = 0; ii < _createdGrids.Count; ii++) {
+            DestroyImmediate(_createdGrids[ii].gameObject);
+        }
+
+        _createdGrids = new List<Grid>();
     }
 
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(GridSystem))]
+public class GridSystemEditor : Editor {
+    public GridSystem gridSystem;
+
+    public void OnEnable() {
+        gridSystem = (GridSystem)target;
+    }
+
+    public override void OnInspectorGUI() {
+        GUI.backgroundColor = Color.green;
+        GUILayout.Space(10f);
+        GUILayout.Label("GRID SYSTEM");
+        if (GUILayout.Button("CREATE GRIDS")) {
+            if (gridSystem.IsCreated) {
+                gridSystem.DeleteGrids();
+            }
+
+            gridSystem.CreateGrids();
+
+            EditorWindow view = EditorWindow.GetWindow<SceneView>();
+            view.Repaint();
+        }
+        GUILayout.Space(10f);
+        GUI.backgroundColor = Color.red;
+        if (GUILayout.Button("DELETE GRIDS")) {
+            gridSystem.DeleteGrids();
+
+            EditorWindow view = EditorWindow.GetWindow<SceneView>();
+            view.Repaint();
+        }
+        GUI.backgroundColor = Color.cyan;
+
+        base.OnInspectorGUI();
+    }
+}
+#endif
