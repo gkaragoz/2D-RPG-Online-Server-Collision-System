740ca538b81fdc6ed60462f5d929009955b2aac9
gkaragoz <gkaragoz96@gmail.com>
2019-01-26 14:23:55 +0300

    Preparations of basics.

---
+++ 2D-Primitive-Collision-System/Assets/Scripts/GridSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour {
+
+    public GameObject groundObject;
+
+    [SerializeField]
+    private List<Grid> _grids = new List<Grid>();
+
+    [SerializeField]
+    private float WIDTH = 10;
+    [SerializeField]
+    private float HEIGHT = 10;
+
+    private void Awake() {
+        Mesh mesh = groundObject.GetComponent<MeshFilter>().mesh;
+
+        WIDTH = mesh.bounds.size.x;
+        HEIGHT = mesh.bounds.size.z;
+    }
+
+}
